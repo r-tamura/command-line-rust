fd59e902d5972e8c7aea50d946ed6175f78d87b6
r-tamura <rtamura30@gmail.com>
2024-11-03 09:03:34 +0000

    Chapter 6(uniqr): 複数行対応

--- 06_uniqr/src/lib.rs
+++ 06_uniqr/src/lib.rs
@@ -22,6 +22,17 @@ pub struct Config {
     pub debug: bool,
 }
 
+impl Config {
+    pub fn new(in_file: Option<String>, out_file: Option<String>, count: bool) -> Self {
+        Config {
+            in_file,
+            out_file,
+            count,
+            debug: false,
+        }
+    }
+}
+
 impl From<Args> for Config {
     fn from(args: Args) -> Self {
         Config {
@@ -70,33 +81,102 @@ fn open(filepath: &str) -> Result<Box<dyn std::io::BufRead>, UniqrError> {
     }
 }
 
-pub fn run(config: Config) -> Result<String, UniqrError> {
-    let mut reader = open(config.in_file.unwrap_or("-".to_string()).as_str())?;
+fn format_line(count: u32, line: &str, show_count: bool) -> String {
+    if show_count {
+        format!("{:4} {}", count, line)
+    } else {
+        line.to_string()
+    }
+}
+
+pub fn _run<R: std::io::BufRead>(config: &Config, r: R) -> Result<String, UniqrError> {
+    let mut reader = r;
     let mut line = String::new();
 
     let mut result = String::new();
     let mut prev_line = String::new();
     let mut count = 1;
+
+    let read = reader.read_line(&mut prev_line)?;
+    if read == 0 {
+        return Ok(result);
+    }
+
     loop {
         let read = reader.read_line(&mut line)?;
-        if prev_line == line {
-            count += 1;
-        }
 
         if read == 0 {
-            break;
+            result.push_str(&format_line(count, prev_line.as_str(), config.count));
+            return Ok(result);
         }
+
+        while prev_line.trim() == line.trim() {
+            count += 1;
+            line.clear();
+            let read = reader.read_line(&mut line)?;
+            if read == 0 {
+                result.push_str(&format_line(count, prev_line.as_str(), config.count));
+                return Ok(result);
+            }
+        }
+
+        result.push_str(&format_line(count, prev_line.as_str(), config.count));
 
         prev_line = line.clone();
-
-        if config.count {
-            result.push_str(format!("{:4} {}", count, prev_line).as_str());
-        } else {
-            result.push_str(format!("{}", prev_line).as_str());
-        }
         line.clear();
         count = 1;
     }
+}
 
-    Ok(result)
+pub fn run(config: Config) -> Result<String, UniqrError> {
+    let in_file = (&config).in_file.clone().unwrap_or("-".to_string());
+    let mut reader = open(in_file.as_str())?;
+    _run(&config, reader.as_mut())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Cursor;
+
+    #[test]
+    fn test_two_count_same() {
+        let config = Config {
+            in_file: None,
+            out_file: None,
+            count: true,
+            debug: false,
+        };
+        let input = "a\na\n";
+        let expected = "   2 a\n";
+        let actual = _run(&config, Cursor::new(input)).unwrap();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_two_count_different() {
+        let config = Config::new(None, None, true);
+        let input = "a\nb\n";
+        let expected = "   1 a\n   1 b\n";
+        let actual = _run(&config, Cursor::new(input)).unwrap();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_three_count_different() {
+        let config = Config::new(None, None, true);
+        let input = "a\na\nb\n";
+        let expected = "   2 a\n   1 b\n";
+        let actual = _run(&config, Cursor::new(input)).unwrap();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_改行コードがある行とない行では区別されない() {
+        let config = Config::new(None, None, true);
+        let input = "a\na";
+        let expected = "   2 a\n";
+        let actual = _run(&config, Cursor::new(input)).unwrap();
+        assert_eq!(actual, expected);
+    }
 }
