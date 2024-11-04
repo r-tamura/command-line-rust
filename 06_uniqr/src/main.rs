e3ed3009fa58d71a9138ec9c5f211a763069607f
r-tamura <rtamura30@gmail.com>
2024-11-04 04:32:40 +0000

    Chapter 6(uniqr): リファクタリング
    書籍の実装例にあった、Writeトレイトを使った出力方式へ変更

--- 06_uniqr/src/main.rs
+++ 06_uniqr/src/main.rs
@@ -1,15 +1,18 @@
-fn main() {
+use std::io::Write;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = uniqr::get_args().expect("Error parsing arguments");
     let debug = config.debug;
     match uniqr::run(config.clone()) {
-        Ok(result) => match config.out_file {
-            Some(out_file) => {
-                std::fs::write(out_file, result).expect("Error writing to file");
-            }
-            None => {
-                print!("{}", result)
-            }
-        },
+        Ok(result) => {
+            let mut out_file: Box<dyn Write> = match config.out_file {
+                Some(out_file) => Box::new(std::fs::File::create(out_file)?),
+                None => Box::new(std::io::stdout()),
+            };
+
+            write!(out_file, "{}", result)?;
+            Ok(())
+        }
         Err(e) => {
             eprintln!("Error: {}", e);
             if debug == true {
